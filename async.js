48711e7295b57f897b3db980ad0b513dcc363b98
Prnx-l <pranjal0556@gmail.com>
2026-06-27 11:49:59 +0545

    Day 3: async await and fetch practice

--- async.js
+++ async.js
@@ -1,38 +1,51 @@
-async function getData() {
-  const response = await fetch("https://someapi.com/data");
-  const data = await response.json();
-  console.log(data);
-}
+// async function getData() {
+//   const response = await fetch("https://someapi.com/data");
+//   const data = await response.json();
+//   console.log(data);
+// }
 
-getData();
+// getData();
 
-async function getData() {
-  const response = await fetch("https://someapi.com/data");
-  const data = await response.json();
-  console.log(data);
-}
+// async function getData() {
+//   const response = await fetch("https://someapi.com/data");
+//   const data = await response.json();
+//   console.log(data);
+// }
 
-getData();
+// getData();
 
-async function getAllUsers() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+// async function getAllUsers() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const users = await response.json();
 
-  // now use your array skills!
-  const names = users.map((user) => user.name);
-  console.log(names);
-}
+//   // now use your array skills!
+//   const names = users.map((user) => user.name);
+//   console.log(names);
+// }
 
-getAllUsers();
+// getAllUsers();
 
-async function getUsers() {
+// async function getUsers() {
+//   try {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const users = await response.json();
+//     console.log(users);
+//   } catch (error) {
+//     console.log("Something went wrong:", error);
+//   }
+// }
+
+// getUsers();
+
+async function getJobs() {
   try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
-    console.log(users);
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const posts = await response.json();
+    const firstFive = posts.slice(0, 5);
+    const mapped = firstFive.map((job) => ({ title: job.title, id: job.id }));
+    console.log(mapped);
   } catch (error) {
-    console.log("Something went wrong:", error);
+    console.log("something went wrong", error);
   }
 }
-
-getUsers();
+getJobs();
